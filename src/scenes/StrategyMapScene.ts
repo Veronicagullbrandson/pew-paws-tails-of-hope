1008be185948c146279685ba8559052452cce957
lika4509 <linn.kallstrom@protonmail.com>
2024-04-06 17:05:53 +0200

    Added a test strategy map

--- src/scenes/StrategyMapScene.ts
+++ src/scenes/StrategyMapScene.ts
@@ -2,16 +2,33 @@ import Phaser from 'phaser';
 import { GridEngine } from 'grid-engine';
 
 export class StrategyMapScene extends Phaser.Scene {
+    private gridEngine!: GridEngine;
 
     constructor() {
       super('StrategyMapScene');
     }
 
     preload() {
-      this.load.image('grass', 'assets/tiles/grass.png');
+        this.load.image("tiles", "assets/tiles/punyworld-overworld-tileset.png");
+        this.load.tilemapTiledJSON("map", "maps/TestTilemap2.json");
+        
     }
 
     create() {
-        this.add.image(100, 100, 'grass');
+        const map = this.make.tilemap({ key: "map" });
+        const tileset = map.addTilesetImage("Tileset", "tiles");
+        const floor = map.createLayer('Floor', tileset, 0, 0);
+        const wall = map.createLayer('Collision', tileset, 0, 0);
+        const gridEngineConfig = {
+          characters: [
+            //{
+            //  id: "player",
+            //  sprite: playerSprite,
+            //  walkingAnimationMapping: 6,
+            //  startPosition: { x: 3, y: 3 },
+            //},
+          ],
+        };
+        this.gridEngine.create(map, gridEngineConfig);
     }
 }
