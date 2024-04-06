8b0ea01b73a0cae5e55e928ee6245ebac001d36a
ellabellaplingplong <ella.lans.3245@gmail.com>
2024-04-06 13:46:39 +0200

    added physics (:

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import Phaser from 'phaser';
 import { WelcomeScene } from './scenes/WelcomeScene';
 import { StrategyMapScene } from './scenes/StrategyMapScene';
+import { CharacterTestScene } from './scenes/CharacterTestScene';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  scene: [WelcomeScene, StrategyMapScene],
+  scene: [WelcomeScene, StrategyMapScene, CharacterTestScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+    }
+  }
 };
 
 new Phaser.Game(config);
